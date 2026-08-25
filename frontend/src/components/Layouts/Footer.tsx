1b2959ea09d2e061992bc2db7c5719fa9722c37d
mohamuddev <maxamuud632@gmail.com>
2026-08-25 16:22:00 +0300

    added siiv icon to footer

--- frontend/src/components/Layouts/Footer.tsx
+++ frontend/src/components/Layouts/Footer.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../store';
+
 const Footer = () => {
-    return <div className="dark:text-white-dark text-center ltr:sm:text-left rtl:sm:text-right p-6 pt-0 mt-auto">© {new Date().getFullYear()} MaamulPro. Platform administration.</div>;
+    const isDarkMode = useSelector((state: IRootState) => state.themeConfig.isDarkMode);
+    return (
+      <div className="mt-auto flex items-center justify-center gap-1.5 p-6 pt-0 text-center">
+    <img
+        src={
+            isDarkMode
+                ? '/assets/images/siiv-dark-theme.png'
+                : '/assets/images/siiv-light-theme.png'
+        }
+        alt="SIIV"
+        className="h-4 w-auto"
+    />
+
+    <span className="whitespace-nowrap text-sm font-medium text-gray-600 dark:text-gray-400">
+        © {new Date().getFullYear()} SIIV Group
+    </span>
+</div>
+    );
 };
 
 export default Footer;
